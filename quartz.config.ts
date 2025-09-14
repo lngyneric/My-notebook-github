9709c82f2d438d22d824c9bc212e961ab3fd0568
ling.yun <lingyun@sysmex.com.cn>
2025-09-14 14:38:40 +0800

    Move markdown files to content directory and hide docs folder

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "lngyneric.github.io/My-notebook-github",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "docs"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
